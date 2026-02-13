0071a7f6e052c07f4e87516da45c985d0aa16a99
Kentypa <12orator12@gmail.com>
2026-02-13 11:01:28 +0000

    FIX

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  base: "/ETI-React-Learing-Again/",
   plugins: [
     tanstackRouter({
       target: "react",
