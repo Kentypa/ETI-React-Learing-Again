79b5901486686f03fc07ef85125f86632c5b2805
Kentypa <12orator12@gmail.com>
2026-02-18 16:19:56 +0200

    Fixes

--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { postsData } from "../mock/mock-data";
+import { postsData, type PostType } from "../mock/mock-data";
 import { Post } from "../shared/components/molecules/Post/Post";
 import { useState } from "react";
 import { SearchBar } from "../shared/components/molecules/SearchBar";
@@ -11,6 +11,10 @@ export const Route = createFileRoute("/")({
 function HomeComponent() {
   const [currentTag, setCurrentTag] = useState<string>("");
 
+  const filteredPosts: PostType[] = postsData.filter((post) =>
+    post.tag.toLowerCase().includes(currentTag.toLowerCase()),
+  );
+
   return (
     <main className="flex flex-col justify-center items-center p-10 bg-[#f0f2f5]">
       <h1 className="text-2xl font-bold mb-12">News List</h1>
@@ -21,15 +25,15 @@ function HomeComponent() {
       />
 
       <ul className="flex flex-col gap-4 border-2 p-4 rounded-md">
-        {postsData
-          .filter((post) =>
-            post.tag.toLowerCase().includes(currentTag.toLowerCase()),
-          )
-          .map((post) => (
+        {filteredPosts.length === 0 ? (
+          <p>No posts by this tag</p>
+        ) : (
+          filteredPosts.map((post) => (
             <li key={post.id}>
               <Post {...post} />
             </li>
-          ))}
+          ))
+        )}
       </ul>
     </main>
   );
