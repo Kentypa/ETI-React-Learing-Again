dfa0c112b318d370a9f5c24d6e6d250ab2b259ff
Kentypa <12orator12@gmail.com>
2026-02-25 16:44:03 +0200

    feat: implement student form with validation.

--- apps/frontend/src/routes/students.tsx
+++ apps/frontend/src/routes/students.tsx
@@ -1,10 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { StudentsList } from "../shared/components/molecules/StudentList";
-import { studentsData } from "../mock/students-data";
-import { StudentsFilteredList } from "../shared/components/molecules/StudentFilteredList";
-import { StudentsAvarageList } from "../shared/components/molecules/StudentAvarageScoreList";
 import { useState } from "react";
+import { studentsData } from "../mock/students-data";
 import { Button } from "../shared/components/atoms/Button/Button";
+import { AddStudentForm } from "../shared/components/molecules/AddStudentForm/AddStudentForm";
+import { StudentsAverageList } from "../shared/components/molecules/StudentAverageScoreList";
+import { StudentsFilteredList } from "../shared/components/molecules/StudentFilteredList";
+import { StudentsList } from "../shared/components/molecules/StudentList";
 
 export const Route = createFileRoute("/students")({
   component: RouteComponent,
@@ -12,6 +13,7 @@ export const Route = createFileRoute("/students")({
 
 function RouteComponent() {
   const [filterActive, setFilterActive] = useState(false);
+  const [students, setStudents] = useState(studentsData)
 
   return (
     <main className="flex gap-10 justify-center items-center bg-[#f0f2f5] p-10">
@@ -26,17 +28,23 @@ function RouteComponent() {
         <div className="flex flex-col gap-6 text-center">
           <h3>Active Students</h3>
           <StudentsFilteredList
-            students={studentsData}
+            students={students}
             cb={(student) => (student.score || 0) >= 60}
           />
         </div>
       ) : (
         <div className="flex flex-col gap-6 text-center">
           <h2>All students</h2>
-          <StudentsList students={studentsData} />
+          <StudentsList students={students} />
         </div>
       )}
-      <StudentsAvarageList students={studentsData} />
+      <StudentsAverageList students={students} />
+      <AddStudentForm
+        onAdd={(s) => {
+          console.log(s);
+          setStudents([...students, s])
+        }}
+      />
     </main>
   );
 }
