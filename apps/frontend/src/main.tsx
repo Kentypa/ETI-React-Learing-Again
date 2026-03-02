4d01945bd7673f4e906b33bfcd4eebba15cc6acb
Kentypa <12orator12@gmail.com>
2026-03-02 11:25:13 +0200

    not found

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,15 +1,17 @@
-import "./styles.css";
-import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
-import { StrictMode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
+import { NotFound } from "./shared/components/pages/NotFound";
+import "./styles.css";
 
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
   scrollRestoration: true,
   basepath: "/ETI-React-Learing-Again/",
+  defaultNotFoundComponent: () => <NotFound />,
 });
 
 declare module "@tanstack/react-router" {
---
+++ apps/frontend/src/shared/components/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+export const NotFound: FC = () => {
+  return (
+    <div className="h-dvh flex justify-center items-center text-9xl">
+      NOT FOUND 404 <span className="animate-spin">+</span>
+    </div>
+  );
+};
