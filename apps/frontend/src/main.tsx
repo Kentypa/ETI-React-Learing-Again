26bff6b6ecfb5ef2a44f620f6462eadfd91194f2
Kentypa <12orator12@gmail.com>
2026-03-02 11:37:24 +0200

    not found

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,15 +1,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import {
+  RouterProvider,
+  createHashHistory,
+  createRouter,
+} from "@tanstack/react-router";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen";
 import { NotFound } from "./shared/components/pages/NotFound";
 import "./styles.css";
 
+const hashHistory = createHashHistory();
+
 const router = createRouter({
   routeTree,
   defaultPreload: "intent",
   scrollRestoration: true,
+  history: hashHistory,
   basepath: "/ETI-React-Learing-Again/",
   defaultNotFoundComponent: () => <NotFound />,
 });
